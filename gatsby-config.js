a6748760a2b5321205a740f931749a27fa01c55c
Mat Jordan <mat@utk.edu>
2021-06-07 13:29:08 -0400

    Restore icon for build.

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `src/media/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
