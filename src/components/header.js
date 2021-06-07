06d85b899294ad210d014fd3fbc840cec0e1e066
Mat Jordan <mat@utk.edu>
2021-06-07 12:30:32 -0400

    Test base layout.

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,45 @@
 import * as React from "react"
-import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
 import { Link } from "gatsby"
+import PropTypes from "prop-types"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <>
+    <Helmet>
+      <title>Exhibits Demo</title>
+      <script crossOrigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+      <script crossOrigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+      <script defer src="https://www.lib.utk.edu/assets/universal-header.js?v=1.3.0" async type="text/javascript" />
+      <script src="https://yith.netlify.app/yith.js" async type="text/javascript" />
+    </Helmet>
+    <header id="utk-lib-header" data-page-title="Exhibits | Libraries at University of Tennessee, Knoxville" />
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
     </div>
-  </header>
+  </>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,23 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
+    <div className="yith yith-projection" id="flowers-projection" data-mode="projection">
+      <a className="yith-expand" href="#">Heilman's Flora</a>
+      <div className="yith-structure">
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/heilman/197">
+          <figcaption>
+            <b>Butterfly Maple, Acer japonicum</b>
+          </figcaption>
+        </figure>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/heilman/264">
+          <figcaption>
+            <b>Red Maple, Acer rubrum</b>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
   </Layout>
 )
 
