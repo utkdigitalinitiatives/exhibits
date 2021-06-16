e1b9b17dd04b08f615180e18e0eb349c75242468
Sarah Funk <sfunk3@utk.edu>
2021-06-16 13:28:07 -0400

    Filling out data in Galston timeline.

--- src/pages/galston/gottfried-galston.js
+++ src/pages/galston/gottfried-galston.js
@@ -13,19 +13,15 @@ const GalstonPage = () => (
           mode="chronology">
       <div className="yith-structure">
         <span>1879</span>
-        <span>1900</span>
-        <span>1900</span>
-        <span>1900</span>
-        <span>1900</span>
-        <span>1900</span>
-        <span>1900</span>
-        <span>1900</span>
+        <span>1899</span>
         <span>1901</span>
         <span className="yith-date">1902</span>
         <span className="yith-emblem">1902</span>
         <figure className="yith-manifest"
                 data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/692"></figure>
         <span>1903</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/726"></figure>
         <span>1904</span>
         <span className="yith-date">1905</span>
         <span className="yith-emblem">1905</span>
@@ -41,7 +37,29 @@ const GalstonPage = () => (
                 data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/703"
                 data-region="1050,600,2000,1400"></figure>
         <span>1909</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/721"></figure>
         <span>1910</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/724"></figure>
+        <span>1912</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/725"></figure>
+        <span>1915</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/728"></figure>
+        <span>1919</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/729"></figure>
+        <span>1921</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/697"></figure>
+        <span>1927</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/690"></figure>
+        <span>1950</span>
+        <figure className="yith-manifest"
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/688"></figure>
       </div>
     </Yith>
 
