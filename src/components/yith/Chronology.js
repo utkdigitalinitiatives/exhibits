8f91fae0f7a8d56d731c3ad86b9b9dbcc708fa19
Mat Jordan <mat@utk.edu>
2021-06-21 09:35:53 -0400

    Reformat, prettier.

--- src/components/yith/Chronology.js
+++ src/components/yith/Chronology.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react';
-import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller} from 'react-scroll'
-import Sticky from 'react-sticky-el';
+import React, { Component } from "react"
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll"
+import Sticky from "react-sticky-el"
 
 import Index from "./Index"
 import Screen from "./Screen"
 
 class Chronology extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       activeIndex: 0,
-      screenTop: 300
-    };
+      screenTop: 300,
+    }
   }
 
-  mapStructure = (sequence) => {
-
+  mapStructure = sequence => {
     return sequence.map((element, index) => {
-      if (element.tag === 'figure') {
-
+      if (element.tag === "figure") {
         let region = null
-        if (typeof element.region === 'string') {
+        if (typeof element.region === "string") {
           region = element.region
         }
 
         let autozoom = false
-        if (typeof element.autozoom === 'string') {
-          if (element.autozoom === 'true') {
+        if (typeof element.autozoom === "string") {
+          if (element.autozoom === "true") {
             autozoom = true
           }
         }
 
-        const name = 'chronology' + index
+        const name = "chronology" + index
 
         return (
-
           <Element name={index} key={index} className={element.class}>
             <h4>{element.label}</h4>
             <p>a bunch of stuff</p>
@@ -45,64 +48,62 @@ class Chronology extends Component {
       } else {
         return null
       }
-    });
+    })
   }
 
-  mapIndex = (sequence) => {
+  mapIndex = sequence => {
     return sequence.map((element, index) => {
-      if (element.tag === 'figure') {
+      if (element.tag === "figure") {
         return (
           <Link to={index} spy={true} onSetActive={this.handleSetActive}>
             {element.label}
           </Link>
         )
       }
-    });
+    })
   }
 
-  handleSetActive = (index) => {
+  handleSetActive = index => {
     if (typeof index !== "undefined") {
       let activeIndex = parseInt(index)
       if (Number.isInteger(activeIndex)) {
         this.setState({
-          activeIndex: activeIndex
+          activeIndex: activeIndex,
         })
       }
     }
   }
 
   componentDidMount() {
-    Events.scrollEvent.register('begin', function(to, element) {
+    Events.scrollEvent.register("begin", function (to, element) {
       // console.log('begin', arguments);
-    });
+    })
 
-    Events.scrollEvent.register('end', function(to, element) {
+    Events.scrollEvent.register("end", function (to, element) {
       // console.log('end', arguments);
-    });
+    })
 
-    scrollSpy.update();
+    scrollSpy.update()
 
-    window.addEventListener("scroll", ()=> {
-      var yithIndex = document.getElementById('yith-index');
-      var distanceToTop = yithIndex.getBoundingClientRect().top;
+    window.addEventListener("scroll", () => {
+      var yithIndex = document.getElementById("yith-index")
+      var distanceToTop = yithIndex.getBoundingClientRect().top
 
       this.setState({
-        screenTop: distanceToTop + yithIndex.offsetHeight
+        screenTop: distanceToTop + yithIndex.offsetHeight,
       })
-    });
-
+    })
   }
 
   componentWillUnmount() {
-    Events.scrollEvent.remove('begin');
-    Events.scrollEvent.remove('end');
+    Events.scrollEvent.remove("begin")
+    Events.scrollEvent.remove("end")
   }
 
   render() {
+    const { sequence } = this.props
 
-    const {sequence} = this.props
-
-    let screenHeight = 'calc(100% - ' + this.state.screenTop + 'px)';
+    let screenHeight = "calc(100% - " + this.state.screenTop + "px)"
 
     return (
       <React.Fragment>
@@ -115,17 +116,13 @@ class Chronology extends Component {
           <aside className="yith-structure">
             {this.mapStructure(sequence)}
           </aside>
-          <div className="yith-screen"
-               style={{height: screenHeight}}>
-            <Screen activeIndex={this.state.activeIndex}
-                    data={sequence} />
+          <div className="yith-screen" style={{ height: screenHeight }}>
+            <Screen activeIndex={this.state.activeIndex} data={sequence} />
           </div>
         </div>
       </React.Fragment>
     )
-
   }
-
 }
 
-export default Chronology;
+export default Chronology
--- src/pages/galston/gottfried-galston.js
+++ src/pages/galston/gottfried-galston.js
@@ -9,49 +9,72 @@ const GalstonPage = () => (
   <Layout exhibit="galston">
     <Seo title="Gottfried Galston" />
 
-		<section className="exhibits-section">
-			<div>
-				<h2>Gottfied Galston, 1879-1950</h2>
-				<p>At the heart of this exhibit is a unique, bound volume of a groundbreaking publication in music pedagogy - <Link to="/galston/studienbuch" activeClassName="active">Gottfried Galston’s Studienbuch</Link>. The exhibit features the first edition of the Austrian-born pianist’s German-language book (1910), accompanied by a specially-commissioned--and previously unpublished--English translation.</p>
-				<p>In his pioneering study book, Galston provides a thorough record of his thought processes and musical interpretations for the works of five composers - Bach, Beethoven, Liszt, Brahms, and Chopin. </p>
-			</div>
-			<div>
-				<Yith id="galston-intro"
-							mode="present">
-					<div className="yith-structure">
-						<figure className="yith-manifest"
-										data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/680"></figure>
-					</div>
-				</Yith>
-			</div>
-		</section>
-    <Yith id="studienbuch-chronology"
-          mode="chronology">
-			<div className="yith-structure">
-				<figure className="yith-manifest"
-								data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/684"
-								data-label="1879"></figure>
-				<figure className="yith-manifest"
-						data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/692"
-						data-label="1900"></figure>
-				<figure className="yith-manifest"
-						data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/710"
-						data-label="1903"
-						data-region="1338,1238,2902,3479"
-						data-autozoom="true"></figure>
-				<figure className="yith-manifest"
-						data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/700"
-						data-label="1905"
-						data-region="2450,1400,3000,2300"
-						data-autozoom="true"></figure>
-				<figure className="yith-manifest"
-						data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/694"
-						data-label="1907"
-						data-region="2168,729,3947,2732"
-						data-autozoom="true"></figure>
-			</div>
+    <section className="exhibits-section">
+      <div>
+        <h2>Gottfied Galston, 1879-1950</h2>
+        <p>
+          At the heart of this exhibit is a unique, bound volume of a
+          groundbreaking publication in music pedagogy -{" "}
+          <Link to="/galston/studienbuch" activeClassName="active">
+            Gottfried Galston’s Studienbuch
+          </Link>
+          . The exhibit features the first edition of the Austrian-born
+          pianist’s German-language book (1910), accompanied by a
+          specially-commissioned--and previously unpublished--English
+          translation.
+        </p>
+        <p>
+          In his pioneering study book, Galston provides a thorough record of
+          his thought processes and musical interpretations for the works of
+          five composers - Bach, Beethoven, Liszt, Brahms, and Chopin.{" "}
+        </p>
+      </div>
+      <div>
+        <Yith id="galston-intro" mode="present">
+          <div className="yith-structure">
+            <figure
+              className="yith-manifest"
+              data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/680"
+            ></figure>
+          </div>
+        </Yith>
+      </div>
+    </section>
+    <Yith id="studienbuch-chronology" mode="chronology">
+      <div className="yith-structure">
+        <figure
+          className="yith-manifest"
+          data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/684"
+          data-label="1879"
+        ></figure>
+        <figure
+          className="yith-manifest"
+          data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/692"
+          data-label="1900"
+        ></figure>
+        <figure
+          className="yith-manifest"
+          data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/710"
+          data-label="1903"
+          data-region="1338,1238,2902,3479"
+          data-autozoom="true"
+        ></figure>
+        <figure
+          className="yith-manifest"
+          data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/700"
+          data-label="1905"
+          data-region="2450,1400,3000,2300"
+          data-autozoom="true"
+        ></figure>
+        <figure
+          className="yith-manifest"
+          data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/694"
+          data-label="1907"
+          data-region="2168,729,3947,2732"
+          data-autozoom="true"
+        ></figure>
+      </div>
     </Yith>
-
   </Layout>
 )
 
