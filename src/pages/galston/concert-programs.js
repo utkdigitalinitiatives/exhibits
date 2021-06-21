5d3155226aa8b35029c0b1b80f473a771be14393
Sarah Funk <sfunk3@utk.edu>
2021-06-21 17:02:40 -0400

    Adding present function and text to concert programs page.

--- src/pages/galston/concert-programs.js
+++ src/pages/galston/concert-programs.js
@@ -10,14 +10,16 @@ const ConcertProgramsPage = () => (
     <Seo title="Concert Programs" />
     <h1>Concert Programs</h1>
 
-    <Yith mode="comparison">
-      <a className="yith-expand" href="#">Expand for Comparative Analysis</a>
+    <p>This is a bound volume of the printed programs from each of Galston’s forty concert cycles between 1919 and 1921. The volume was likely bound especially for Galston, and bears his “Ex Libris” plate on the inside front cover.</p>
+
+    <p>The programs in this volume can be compared with the handwritten lists of each concert that Galston wrote in the back of the Studienbuch bound volume.</p>
+
+    <Yith id="studienbuch-present-test"
+          mode="present">
       <div className="yith-structure">
         <figure className="yith-manifest"
-                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/178"
-                data-canvas="https://digital.lib.utk.edu/assemble/manifest/galston/178/canvas/2"></figure>
-        <figure className="yith-manifest"
-                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/736"></figure>
+                data-manifest="https://digital.lib.utk.edu/assemble/manifest/galston/1"
+                data-canvas="https://digital.lib.utk.edu/assemble/manifest/galston/1/canvas/175"></figure>
       </div>
     </Yith>
   </Layout>
