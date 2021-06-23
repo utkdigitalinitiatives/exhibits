43a338bc396a52e04dd6c4be2244fb1f8948076d
Mat Jordan <mat@utk.edu>
2021-06-23 13:21:47 -0400

    Update sample components for testing.

--- src/pages/sample/present.js
+++ src/pages/sample/present.js
@@ -6,19 +6,33 @@ import Seo from "../../components/seo"
 import Yith from "../../components/yith/Yith"
 
 const Present = () => (
-
   <Layout exhibit="sample">
     <Seo title="Present: Sample Exhibit" />
 
     <section className="exhibits-section">
       <div>
         <h2>Knoxville, The Early Years</h2>
-        <p>Sed vitae dui ut urna suscipit lobortis id ac magna. Donec velit ex, dapibus a maximus vel, feugiat vel lacus. Maecenas maximus, lorem in aliquet ultricies, nisi metus tincidunt ligula, eget viverra massa metus at sapien. Ut ullamcorper id mauris luctus maximus. Nulla maximus lectus dui. Phasellus blandit tristique sem. Etiam et ex vel metus mattis rutrum.</p>
-        <p>Cras condimentum ornare velit, nec malesuada augue efficitur a. Donec mi orci, eleifend eget laoreet sit amet, consectetur in quam. Vivamus eleifend nunc nec ante porta, et consequat est semper. Aliquam ullamcorper dui quis diam mollis, sed posuere est efficitur.</p>
-        <p>Duis finibus dui lectus, id aliquam dolor ornare eu. Curabitur ut pellentesque nisi, at pretium odio.</p>
+        <p>
+          Sed vitae dui ut urna suscipit lobortis id ac magna. Donec velit ex,
+          dapibus a maximus vel, feugiat vel lacus. Maecenas maximus, lorem in
+          aliquet ultricies, nisi metus tincidunt ligula, eget viverra massa
+          metus at sapien. Ut ullamcorper id mauris luctus maximus. Nulla
+          maximus lectus dui. Phasellus blandit tristique sem. Etiam et ex vel
+          metus mattis rutrum.
+        </p>
+        <p>
+          Cras condimentum ornare velit, nec malesuada augue efficitur a. Donec
+          mi orci, eleifend eget laoreet sit amet, consectetur in quam. Vivamus
+          eleifend nunc nec ante porta, et consequat est semper. Aliquam
+          ullamcorper dui quis diam mollis, sed posuere est efficitur.
+        </p>
+        <p>
+          Duis finibus dui lectus, id aliquam dolor ornare eu. Curabitur ut
+          pellentesque nisi, at pretium odio.
+        </p>
       </div>
       <div>
-        <Yith id="galston-intro" mode="present">
+        <Yith id="sample-present-1" mode="present">
           <div className="yith-structure">
             <figure
               className="yith-manifest"
@@ -31,7 +45,7 @@ const Present = () => (
 
     <section className="exhibits-section">
       <div>
-        <Yith id="galston-intro" mode="present">
+        <Yith id="sample-present-2" mode="present">
           <div className="yith-structure">
             <figure
               className="yith-manifest"
@@ -42,14 +56,27 @@ const Present = () => (
       </div>
       <div>
         <h2>Tennessee River</h2>
-        <p>Sed vitae dui ut urna suscipit lobortis id ac magna. Donec velit ex, dapibus a maximus vel, feugiat vel lacus. Maecenas maximus, lorem in aliquet ultricies, nisi metus tincidunt ligula, eget viverra massa metus at sapien. Ut ullamcorper id mauris luctus maximus. Nulla maximus lectus dui. Phasellus blandit tristique sem. Etiam et ex vel metus mattis rutrum.</p>
-        <p>Cras condimentum ornare velit, nec malesuada augue efficitur a. Donec mi orci, eleifend eget laoreet sit amet, consectetur in quam. Vivamus eleifend nunc nec ante porta, et consequat est semper. Aliquam ullamcorper dui quis diam mollis, sed posuere est efficitur.</p>
-        <p>Duis finibus dui lectus, id aliquam dolor ornare eu. Curabitur ut pellentesque nisi, at pretium odio.</p>
+        <p>
+          Sed vitae dui ut urna suscipit lobortis id ac magna. Donec velit ex,
+          dapibus a maximus vel, feugiat vel lacus. Maecenas maximus, lorem in
+          aliquet ultricies, nisi metus tincidunt ligula, eget viverra massa
+          metus at sapien. Ut ullamcorper id mauris luctus maximus. Nulla
+          maximus lectus dui. Phasellus blandit tristique sem. Etiam et ex vel
+          metus mattis rutrum.
+        </p>
+        <p>
+          Cras condimentum ornare velit, nec malesuada augue efficitur a. Donec
+          mi orci, eleifend eget laoreet sit amet, consectetur in quam. Vivamus
+          eleifend nunc nec ante porta, et consequat est semper. Aliquam
+          ullamcorper dui quis diam mollis, sed posuere est efficitur.
+        </p>
+        <p>
+          Duis finibus dui lectus, id aliquam dolor ornare eu. Curabitur ut
+          pellentesque nisi, at pretium odio.
+        </p>
       </div>
     </section>
-
   </Layout>
-
 )
 
 export default Present
