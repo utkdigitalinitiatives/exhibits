c8c6db0fe3f111ad25112ec97046a3703c8dda12
Sarah Funk <sfunk3@utk.edu>
2021-08-10 17:09:05 -0400

    Added svg for search icon and fixed spacing & flex of additional header links.

--- src/components/yith/UniversalHeader.js
+++ src/components/yith/UniversalHeader.js
@@ -26,7 +26,11 @@ class UniversalHeader extends Component {
         </div>
 
         <a className="libraries-link" href="https://lib.utk.edu">Libraries</a>
-        <a href="https://lib.utk.edu">Home</a>
+
+        <div className="utk-lib-links">
+          <svg class="svg-icon search-icon" aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><title id="title">Search Icon</title><desc id="desc">A magnifying glass icon.</desc><g class="search-path" fill="none" stroke="#848F91"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg>
+          <a href="https://lib.utk.edu">Home</a>
+        </div>
       </div>
     )
 
