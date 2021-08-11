d6ae3d89b15bc61df54917bc8f2e74795465c4a0
Sarah Funk <sfunk3@utk.edu>
2021-08-11 16:27:54 -0400

    Added links for search and my account. Adjusted smallest responsive styles.

--- src/components/yith/UniversalHeader.js
+++ src/components/yith/UniversalHeader.js
@@ -30,11 +30,9 @@ class UniversalHeader extends Component {
         </div>
 
         <div className="utk-lib-links">
-        
-        <a className="exhibits-search" href="">
-        <svg class="svg-icon search-icon" aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><title id="title">Search Icon</title><desc id="desc">A magnifying glass icon.</desc><g class="search-path" fill="none" stroke="#848F91"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg>
-          <span>Search</span></a>
-          <a href="https://lib.utk.edu"><span>Home</span></a>
+        <a href="https://utk.primo.exlibrisgroup.com/discovery/login?vid=01UTN_KNOXVILLE:01UTK&lang=en">My Account</a>
+        <div className="exhibits-search" >
+          <a href="https://utk.primo.exlibrisgroup.com/discovery/search?vid=01UTN_KNOXVILLE:01UTK&lang=en"><span>Search</span></a></div>
         </div>
       </div>
     )
