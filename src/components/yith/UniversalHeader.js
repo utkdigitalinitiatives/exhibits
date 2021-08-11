58d901ce0881d229a56acbe6e86e6ec52aa8d072
Sarah Funk <sfunk3@utk.edu>
2021-08-11 14:25:01 -0400

    Adding search icon back in and tweaking positioning.

--- src/components/yith/UniversalHeader.js
+++ src/components/yith/UniversalHeader.js
@@ -31,8 +31,10 @@ class UniversalHeader extends Component {
 
         <div className="utk-lib-links">
         
-        <a className="exhibits-search" href="">Search</a>
-          <a href="https://lib.utk.edu">Home</a>
+        <a className="exhibits-search" href="">
+        <svg class="svg-icon search-icon" aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><title id="title">Search Icon</title><desc id="desc">A magnifying glass icon.</desc><g class="search-path" fill="none" stroke="#848F91"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg>
+          <span>Search</span></a>
+          <a href="https://lib.utk.edu"><span>Home</span></a>
         </div>
       </div>
     )
