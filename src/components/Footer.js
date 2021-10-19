8964f97a35e91e2d0a71e2ca0efbce3ea1de6cac
Sarah Funk <sfunk3@utk.edu>
2021-10-19 13:15:02 -0400

    Adding footer files back in.

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="utk-exhibits-footer">
+          Footer content
+      </div>
+    )
+  }
+}
+
+export default Footer
