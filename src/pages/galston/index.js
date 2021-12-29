c0ea80e7cbb7154090b288cfa398709622c9129c
Mark Baggett <mbagget1@utk.edu>
2021-12-28 21:10:33 -0600

    Add website metadata.

--- src/pages/galston/index.js
+++ src/pages/galston/index.js
@@ -19,13 +19,19 @@ const GalstonIndex = () => {
       }
     `
   )
+  const description = "At the heart of this exhibit is a unique, bound volume of a groundbreaking publication in music pedagogy - Gottfried Galston’s Studienbuch. The exhibit features the first edition of the Austrian-born pianist’s German-language book (1910), accompanied by a specially-commissioned--and previously unpublished--English translation. The bound volume includes several editorial changes -- handwritten by Galston -- later incorporated into the book's 2nd edition (1922).\nIn this pioneering studybook, Galston provides a thorough record of his thought processes and musical interpretations for the works of five composers - Bach, Beethoven, Liszt, Brahms, and Chopin. Widely used in German-speaking countries, the studybook is made available here for the first time with an English translation and the hope that it may contribute to music pedagogy in the U.S. and beyond"
   const schema = [
     {
       "@context": "https://schema.org",
       "@type": "WebSite",
       "name": "Galston Studienbuch Exhibit",
       "url": "https://exhibits.lib.utk.edu/galston",
-      "description": site.siteMetadata.description
+      "description": description,
+      "producer": {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "University of Tennessee, Knoxville. Libraries"
+      }
     },
     {
       "@context": "https://schema.org",
@@ -36,7 +42,6 @@ const GalstonIndex = () => {
         "https://twitter.com/utklibraries",
         "http://id.loc.gov/authorities/names/n87808088",
       ],
-      "description": site.siteMetadata.description,
       "logo": UTKLogo
     }
   ]
