750f3cbea2547f14c82108d69d56b81557176be1
Mark Baggett <mbagget1@utk.edu>
2022-04-26 08:24:49 -0400

    Remove unused variables.

--- src/components/yith/TeaserBackground.js
+++ src/components/yith/TeaserBackground.js
@@ -13,9 +13,6 @@ class TeaserBackground extends Component {
       canvasKey = findIndex(data.items, ['id', this.props.canvas]);
     }
 
-    const src = data.items[canvasKey].items[0].items[0].body[0].service['@id'] + '/' + region + '/' + size + '/0/default.jpg'
-    const alt = data.label.en[0]
-
     return (
       <div className="yith-teaser--background">
         
