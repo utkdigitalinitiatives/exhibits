a9b7897388eaa67b11190c9c4d984e5e965ed61a
Mark Baggett <mbagget1@utk.edu>
2022-04-28 14:22:46 -0400

    Remove stray.

--- src/pages/galston/resources.js
+++ src/pages/galston/resources.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import {graphql, useStaticQuery} from "gatsby"
-import UTKLogo from "../../media/utk-logo.png";
+import UTKLogo from "../../media/utk-logo.png"
 
 const ResourcesPage = () => {
   const {site} = useStaticQuery(
